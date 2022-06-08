c83f6033edc49a2f7bf0795911733775397490c3
youeuneyol <ey.yoo@fanmaum.com>
2022-06-08 15:27:10 +0900

    codelab Compose 기초 완성

--- app/src/main/java/com/dongeul/composesample/ComposeUi.kt
+++ app/src/main/java/com/dongeul/composesample/ComposeUi.kt
@@ -8,7 +8,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
@@ -19,14 +21,20 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dongeul.composesample.ui.theme.ComposeSampleTheme
+import kotlin.math.exp
 
 
 @Composable
@@ -35,43 +43,56 @@ fun Greeting(name: String) {
         mutableStateOf(value = false)
     }
 
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded) 48.dp else 0.dp,
-        animationSpec = tween(
-            durationMillis = 500
-        )
-    )
 
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 9.dp, vertical = 9.dp)
+    Row(
+        Modifier
+            .padding(24.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
-        Row(Modifier.padding(24.dp)) {
-            Column(
-                Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(
-                    text = "Hello"
+        Column(
+            Modifier
+                .padding(12.dp)
+        ) {
+            Text(
+                text = "Hello"
+            )
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
                 )
+            )
+            if (expanded) {
                 Text(
-                    text = name
+                    text = ("Composem ipsum color sit lazy").repeat(4)
                 )
-            }
-
-            OutlinedButton(
-                onClick = { expanded = !expanded },
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(text = if (expanded) "Show Less" else "Show More")
             }
         }
+
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    "Show Less"
+                } else {
+                    "Show More"
+                }
+            )
+
+        }
+
+
     }
 }
 
+
 @Composable
-@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES,name="PreviewGreeting")
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "PreviewGreeting")
 @Preview
 fun previewGreeting() {
     ComposeSampleTheme {
